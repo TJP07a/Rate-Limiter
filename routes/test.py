fa4d156a0d88d263f559f122c494466678106cea
TJP07a <vaibhav_s@es.iitr.ac.in>
2026-02-10 14:29:27 +0530

    Update API endpoint port from 5000 to 0000

--- routes/test.py
+++ routes/test.py
@@ -1,12 +1,12 @@
 import requests
 
 def get_api_key():
-    response = requests.get("http://127.0.0.1:5000/request-api-key")
+    response = requests.get("http://127.0.0.1:0000/request-api-key")
     return response.json()["api_key"]
 
 def send_request():
     API_KEY = get_api_key()
-    response = requests.get("http://127.0.0.1:5000/request-health-status",
+    response = requests.get("http://127.0.0.1:0000/request-health-status",
                         headers= {"X-API-KEY": API_KEY}
                         )
 send_request()
