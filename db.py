59f399c00ce0a7da352522ba78bb22faf7f0473b
TJP07a <vaibhav_s@es.iitr.ac.in>
2026-02-10 14:28:33 +0530

    Change MongoDB connection port to 0000

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient("mongodb://localhost:0000/")
 db = client["Requests_db"]
 requests_collection = db["requests_entries"]
